fc9641065e58483c3619a3ee76431e2bc7e19463
Valerii <valeriikozhevnikov@gmail.com>
2017-06-20 17:04:56 +0300

    Don't working

---
+++ UI-JQuery/UI-JQuery/Models/Car.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UI_JQuery.Models
+{
+    public class Car
+    {
+        public int ID { get; set; }
+        public string Model { get; set; }
+        public string Brand { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public int Mileage { get; set; }
+        public string Color { get; set; }
+        public decimal Price { get; set; }
+    }
+}
